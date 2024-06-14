e2b26dd4133d7b02855c45f81cb64f83b3ac5118
reveuse12 <prayag129787@gmail.com>
2024-06-14 22:46:48 +0530

    add-employee api

--- src/app/api/auth/addemployee/route.js
+++ src/app/api/auth/addemployee/route.js
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 
 export async function GET(request) {
   try {
-    const token = request.cookies.get("token")?.value || "";
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decodedToken, "++++++++++++++++++++11111111111111111111");
-    return NextResponse.json({ decodedToken }, { status: 200 });
+    return NextResponse.json({ message: "added employee" }, { status: 200 });
   } catch (error) {
-    console.log(error);
+    return NextResponse.json(
+      { message: "Internal server error." },
+      { status: 500 }
+    );
   }
 }
